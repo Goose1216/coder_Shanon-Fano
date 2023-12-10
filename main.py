a926f3cc7ce500528310e5ebd990ab031e471f4f
Goose1216 <leha.porublev@mail.ru>
2023-12-10 20:02:48 +0300

    decode

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import os
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
+        self.alph = None
+        self.need_alph = False
         self.pack()
 
         self.frm = tk.Frame()
@@ -40,36 +42,48 @@ class App(tk.Frame):
         self.text_new = ''
 
     def browse_file(self, ):
-        file_dir = filedialog.askopenfilename(title="Выбор файла", initialdir=os.path.curdir, defaultextension='txt',
-                                              filetypes=(("TXT files", "*.txt"),
-                                                         ("All files", "*.*"),))
         try:
-            with open(file_dir, encoding='utf-8') as f:
+            file_dir = filedialog.askopenfilename(title="Выбор файла",
+                                                  initialdir=os.path.curdir,
+                                                  defaultextension='txt',
+                                                  filetypes=(("TXT files", "*.txt"),
+                                                             ("All files", "*.*"),))
+            codecs = ['utf-8', 'ascii', 'cp1251']
+            for codec in codecs:
                 try:
-                    self.text = f.read()
+                    f = open(file_dir, encoding=codec)
                     self.text_field.config(state='normal')
                     self.text_field.delete(0.0, 'end')
+                    self.text = f.read()
                     if len(self.text) > 5000:
                         self.text_field.insert(0.0, self.text[:5000] + '...')
                     else:
                         self.text_field.insert(0.0, self.text)
                     self.text_field.config(state='disabled')
-                except UnicodeDecodeError:
-                    mb.showwarning("ERROR", message='Не поддерживаемый тип файла или в нём кодировка не utf-8')
-        except FileNotFoundError:
+                    f.close()
+                    break
+                except UnicodeDecodeError as e:
+                    if codec == codecs[-1]:
+                        mb.showwarning("ERROR", message='Не поддерживаемый тип файла или в нём кодировка не utf-8')
+                        print(e)
+                    else:
+                        continue
+        except FileNotFoundError as e:
             if file_dir != '':
                 mb.showwarning("ERROR", message=f'Не найдена директория {file_dir}')
+            print(e)
 
     def encode(self):
-        alph = {}
+        self.alph = {}
+        self.need_alph = True
 
-        def search(sequence, num=None):
+        def create_code(sequence, num=None):
             if num is not None:
                 for letter in sequence:
-                    if letter in alph:
-                        alph[letter] += num
+                    if letter in self.alph:
+                        self.alph[letter] += num
                     else:
-                        alph[letter] = num
+                        self.alph[letter] = num
             index = 1
             sum_left = round(probability[0][1], 10)
             sum_right = round((sum((x[1] for x in probability[1: len(sequence)]))), 10)
@@ -86,48 +100,70 @@ class App(tk.Frame):
             sequence1 = sequence[0: index]
             sequence2 = sequence[index: len(sequence)]
             if len(sequence1) > 1:
-                search(sequence1, '1')
+                create_code(sequence1, '1')
             else:
-                if sequence1[0] in alph:
-                    alph[sequence1[0]] += '1'
+                if sequence1[0] in self.alph:
+                    self.alph[sequence1[0]] += '1'
                 else:
-                    alph[sequence1[0]] = '1'
+                    self.alph[sequence1[0]] = '1'
             if len(sequence2) > 1:
-                search(sequence2, '0')
+                create_code(sequence2, '0')
             else:
-                if sequence2[0] in alph:
-                    alph[sequence2[0]] += '0'
+                if sequence2[0] in self.alph:
+                    self.alph[sequence2[0]] += '0'
                 else:
-                    alph[sequence2[0]] = '0'
+                    self.alph[sequence2[0]] = '0'
 
         probability = self.generate_probabilities()
         seq = [word[0] for word in probability]
-        search(seq)
+        create_code(seq)
         self.text_field_new.config(state='normal')
         self.text_field_new.delete(0.0, 'end')
         self.text_new = ''
         for w in self.text:
-            self.text_new += alph[w]
+            self.text_new += self.alph[w]
         if len(self.text_new) > 5000:
             self.text_field_new.insert(0.0, self.text_new[:5000] + '...')
         else:
             self.text_field_new.insert(0.0, self.text_new)
         self.text_field_new.config(state='disabled')
 
-
     def decode(self):
-        pass
+        text = self.text
+        self.need_alph = False
+        text_decode = ''
+        seq = ''
+        end_of_alph = text.find('}')
+        alph = {k[1:-1]: v[1:-1] for v, k in [s.split(': ') for s in text[1:end_of_alph].split(', ')]}
+        print(alph)
+        text = text[end_of_alph+2:]
+        print(text)
+        for w in text:
+            seq += w
+            if seq in alph:
+                word = alph[seq] if alph[seq] != '\\n' else '\n'
+                text_decode += word
+                seq = ''
+        print(text_decode)
+        self.text_field_new.config(state='normal')
+        self.text_field_new.delete(0.0, 'end')
+        self.text_new = text_decode
+        if len(self.text_new) > 5000:
+            self.text_field_new.insert(0.0, self.text_new[:5000] + '...')
+        else:
+            self.text_field_new.insert(0.0, self.text_new)
+        self.text_field_new.config(state='disabled')
 
-    def save_data(self):
+    def save_data(self,):
         f = filedialog.asksaveasfile(mode='w',
-                                    initialfile = 'Decode.txt',
-                                    title="Cохранение файла", initialdir=os.path.curdir,
-                                    defaultextension='.txt',
-                                    filetypes=(("TXT files", "*.txt"),
-                                               ("All files", "*.*"), ))
+                                     initialfile='Decode.txt',
+                                     title="Cохранение файла",
+                                     initialdir=os.path.curdir,
+                                     defaultextension='.txt',
+                                     filetypes=(("TXT files", "*.txt"), ("All files", "*.*"), ))
         if f is None:
             return
-        text_for_save = self.text_new
+        text_for_save = str(self.alph) + '\n' + self.text_new if self.need_alph else self.text_new
         f.write(text_for_save)
         f.close()
 
